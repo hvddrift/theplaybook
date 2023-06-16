2157f95e48dd928e261a2b92edf9a295a0fa867b
hvddrift <hansvanderdrift@gmail.com>
2023-06-16 13:03:10 +1000

    hugo -D

--- public/index.search.js
+++ public/index.search.js
@@ -1,10 +1,213 @@
 var relearn_search_index = [
   {
+    "content": "“Consumers don’t buy products, they buy solutions”\n",
+    "description": "",
+    "tags": null,
+    "title": "3 Pillars",
+    "uri": "/theplaybook/3-pillars/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "_Inex",
+    "uri": "/theplaybook/collection/_inex/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Collections",
+    "uri": "/theplaybook/collection/index.html"
+  },
+  {
     "content": "",
     "description": "",
     "tags": null,
     "title": "My New Hugo Site",
     "uri": "/theplaybook/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Azuma",
+    "uri": "/theplaybook/collection/1-pitt/azuma/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Altar",
+    "uri": "/theplaybook/collection/1-pitt/altar/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Akan",
+    "uri": "/theplaybook/collection/1-pitt/akan/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Air",
+    "uri": "/theplaybook/collection/1-pitt/air/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Bromo",
+    "uri": "/theplaybook/collection/2-pitt/bromo/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Bennett",
+    "uri": "/theplaybook/collection/2-pitt/bennett/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Bely",
+    "uri": "/theplaybook/collection/2-pitt/bely/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Baula",
+    "uri": "/theplaybook/collection/2-pitt/baula/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Baluran",
+    "uri": "/theplaybook/collection/2-pitt/baluran/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Cusin",
+    "uri": "/theplaybook/collection/3-pitt/cusin/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Colo",
+    "uri": "/theplaybook/collection/3-pitt/colo/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Cima",
+    "uri": "/theplaybook/collection/3-pitt/cima/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Capital",
+    "uri": "/theplaybook/collection/3-pitt/capital/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Drum",
+    "uri": "/theplaybook/collection/5-pitt/drum/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Dempo",
+    "uri": "/theplaybook/collection/5-pitt/dempo/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Danau",
+    "uri": "/theplaybook/collection/4-pitt/danau/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Enep",
+    "uri": "/theplaybook/collection/5-pitt/enep/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Elbrus",
+    "uri": "/theplaybook/collection/5-pitt/elbrus/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Ebeko",
+    "uri": "/theplaybook/collection/5-pitt/ebeko/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Foessa",
+    "uri": "/theplaybook/collection/6-pitt/foessa/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "6 Pitt",
+    "uri": "/theplaybook/collection/6-pitt/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "5 Pitt",
+    "uri": "/theplaybook/collection/5-pitt/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "4 Pitt",
+    "uri": "/theplaybook/collection/4-pitt/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "3 Pitt",
+    "uri": "/theplaybook/collection/3-pitt/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "2 Pitt",
+    "uri": "/theplaybook/collection/2-pitt/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "1 Pitt",
+    "uri": "/theplaybook/collection/1-pitt/index.html"
   },
   {
     "content": "h1 Heading 8-) h2 Heading h3 Heading h4 Heading h5 Heading h6 Heading Alternatively, for H1 and H2, an underline-ish style:\nAlt-H1 Alt-H2 Emphasis, aka italics, with asterisks or underscores.\nStrong emphasis, aka bold, with asterisks or underscores.\nCombined emphasis with asterisks and underscores.\nStrikethrough uses two tildes. Scratch this.\nThis is bold text\nThis is bold text\nThis is italic text\nThis is italic text\nStrikethrough\nFirst ordered list item Another item ⋅⋅* Unordered sub-list. Actual numbers don’t matter, just that it’s a number ⋅⋅1. Ordered sub-list And another item. ⋅⋅⋅You can have properly indented paragraphs within list items. Notice the blank line above, and the leading spaces (at least one, but we’ll use three here to also align the raw Markdown).\n⋅⋅⋅To have a line break without a paragraph, you will need to use two trailing spaces.⋅⋅ ⋅⋅⋅Note that this line is separate, but within the same paragraph.⋅⋅ ⋅⋅⋅(This is contrary to the typical GFM line break behaviour, where trailing spaces are not required.)\nUnordered list can use asterisks Or minuses Or pluses Make my changes Fix bug Improve formatting Make the headings bigger Push my commits to GitHub Open a pull request Describe my changes Mention all the members of my team Ask for feedback Create a list by starting a line with +, -, or * Sub-lists are made by indenting 2 spaces: Marker character change forces new list start: Ac tristique libero volutpat at Facilisis in pretium nisl aliquet Nulla volutpat aliquam velit Very easy! ",
