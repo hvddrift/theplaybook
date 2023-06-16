7014074613348f3f18f1e1a662f4e7d723a541c6
dhij <davidhwang.ij@gmail.com>
2023-06-16 03:24:35 +0000

    Updated site

--- index.search.js
+++ index.search.js
@@ -1,5 +1,26 @@
 var relearn_search_index = [
   {
+    "content": "Lorem Ipsum.\n",
+    "description": "",
+    "tags": null,
+    "title": "Pre Install",
+    "uri": "/theplaybook/pre-install/index.html"
+  },
+  {
+    "content": "Hello\n",
+    "description": "",
+    "tags": null,
+    "title": "Install",
+    "uri": "/theplaybook/install/index.html"
+  },
+  {
+    "content": "Now what\n",
+    "description": "",
+    "tags": null,
+    "title": "Post Install",
+    "uri": "/theplaybook/post_install/index.html"
+  },
+  {
     "content": "",
     "description": "",
     "tags": null,
@@ -19,34 +40,6 @@ var relearn_search_index = [
     "tags": null,
     "title": "Akan",
     "uri": "/theplaybook/collection/1-pitt/akan/index.html"
-  },
-  {
-    "content": "",
-    "description": "",
-    "tags": null,
-    "title": "My New Hugo Site",
-    "uri": "/theplaybook/index.html"
-  },
-  {
-    "content": "h1 Heading 8-) h2 Heading h3 Heading h4 Heading h5 Heading h6 Heading Alternatively, for H1 and H2, an underline-ish style:\nAlt-H1 Alt-H2 Emphasis, aka italics, with asterisks or underscores.\nStrong emphasis, aka bold, with asterisks or underscores.\nCombined emphasis with asterisks and underscores.\nStrikethrough uses two tildes. Scratch this.\nThis is bold text\nThis is bold text\nThis is italic text\nThis is italic text\nStrikethrough\nFirst ordered list item Another item ⋅⋅* Unordered sub-list. Actual numbers don’t matter, just that it’s a number ⋅⋅1. Ordered sub-list And another item. ⋅⋅⋅You can have properly indented paragraphs within list items. Notice the blank line above, and the leading spaces (at least one, but we’ll use three here to also align the raw Markdown).\n⋅⋅⋅To have a line break without a paragraph, you will need to use two trailing spaces.⋅⋅ ⋅⋅⋅Note that this line is separate, but within the same paragraph.⋅⋅ ⋅⋅⋅(This is contrary to the typical GFM line break behaviour, where trailing spaces are not required.)\nUnordered list can use asterisks Or minuses Or pluses Make my changes Fix bug Improve formatting Make the headings bigger Push my commits to GitHub Open a pull request Describe my changes Mention all the members of my team Ask for feedback Create a list by starting a line with +, -, or * Sub-lists are made by indenting 2 spaces: Marker character change forces new list start: Ac tristique libero volutpat at Facilisis in pretium nisl aliquet Nulla volutpat aliquam velit Very easy! ",
-    "description": "",
-    "tags": null,
-    "title": "My Second Post",
-    "uri": "/theplaybook/posts/my-second-post/index.html"
-  },
-  {
-    "content": "",
-    "description": "",
-    "tags": null,
-    "title": "Posts",
-    "uri": "/theplaybook/posts/index.html"
-  },
-  {
-    "content": "This is a Test page How does it change in Github?\nDoes this change both\nyes it does\n",
-    "description": "",
-    "tags": null,
-    "title": "My First Post",
-    "uri": "/theplaybook/posts/my-first-post/index.html"
   },
   {
     "content": "",
@@ -199,6 +192,13 @@ var relearn_search_index = [
     "content": "",
     "description": "",
     "tags": null,
+    "title": "Documents",
+    "uri": "/theplaybook/documents/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
     "title": "Drum",
     "uri": "/theplaybook/collection/4-pitt/drum/index.html"
   },
@@ -229,6 +229,27 @@ var relearn_search_index = [
     "tags": null,
     "title": "Foessa",
     "uri": "/theplaybook/collection/6-pitt/foessa/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Partition Construction",
+    "uri": "/theplaybook/partition_construction/index.html"
+  },
+  {
+    "content": " ",
+    "description": "",
+    "tags": null,
+    "title": "PITT cooking Australia Technical",
+    "uri": "/theplaybook/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Service",
+    "uri": "/theplaybook/service/index.html"
   },
   {
     "content": "",
