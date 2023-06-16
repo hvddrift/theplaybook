ffe05c60bdfec5675d8d70504c665af1d04a3f26
dhij <davidhwang.ij@gmail.com>
2023-06-16 03:08:55 +0000

    Updated site

--- index.search.js
+++ index.search.js
@@ -1,10 +1,24 @@
 var relearn_search_index = [
   {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Collection",
+    "uri": "/theplaybook/collection/index.html"
+  },
+  {
     "content": "“Consumers don’t buy products, they buy solutions”\n",
     "description": "",
     "tags": null,
     "title": "3 Pillars",
     "uri": "/theplaybook/3-pillars/index.html"
+  },
+  {
+    "content": " Model Akan Number of burners 1 Power in MJ 1 x 7.2 Main Module 213 x 503 x 90 mm Min. width of cabinet 300 mm Min. depth of benchtop 600 mm Cooking Zone width 227 mm ",
+    "description": "",
+    "tags": null,
+    "title": "Akan",
+    "uri": "/theplaybook/collection/1-pitt/akan/index.html"
   },
   {
     "content": "",
@@ -38,8 +52,183 @@ var relearn_search_index = [
     "content": "",
     "description": "",
     "tags": null,
+    "title": "1 Pitt",
+    "uri": "/theplaybook/collection/1-pitt/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "2 Pitt",
+    "uri": "/theplaybook/collection/2-pitt/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "3 Pitt",
+    "uri": "/theplaybook/collection/3-pitt/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "4 Pitt",
+    "uri": "/theplaybook/collection/4-pitt/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "5 Pitt",
+    "uri": "/theplaybook/collection/5-pitt/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "6 Pitt",
+    "uri": "/theplaybook/collection/6-pitt/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Air",
+    "uri": "/theplaybook/collection/1-pitt/air/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Altar",
+    "uri": "/theplaybook/collection/1-pitt/altar/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Azuma",
+    "uri": "/theplaybook/collection/1-pitt/azuma/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Baluran",
+    "uri": "/theplaybook/collection/2-pitt/baluran/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Baula",
+    "uri": "/theplaybook/collection/2-pitt/baula/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Bely",
+    "uri": "/theplaybook/collection/2-pitt/bely/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Bennett",
+    "uri": "/theplaybook/collection/2-pitt/bennett/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Bromo",
+    "uri": "/theplaybook/collection/2-pitt/bromo/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Capital",
+    "uri": "/theplaybook/collection/3-pitt/capital/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
     "title": "Categories",
     "uri": "/theplaybook/categories/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Cima",
+    "uri": "/theplaybook/collection/3-pitt/cima/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Colo",
+    "uri": "/theplaybook/collection/3-pitt/colo/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Cusin",
+    "uri": "/theplaybook/collection/3-pitt/cusin/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Danau",
+    "uri": "/theplaybook/collection/4-pitt/danau/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Dempo",
+    "uri": "/theplaybook/collection/4-pitt/dempo/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Drum",
+    "uri": "/theplaybook/collection/4-pitt/drum/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Ebeko",
+    "uri": "/theplaybook/collection/5-pitt/ebeko/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Elbrus",
+    "uri": "/theplaybook/collection/5-pitt/elbrus/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Enep",
+    "uri": "/theplaybook/collection/5-pitt/enep/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Foessa",
+    "uri": "/theplaybook/collection/6-pitt/foessa/index.html"
   },
   {
     "content": "",
